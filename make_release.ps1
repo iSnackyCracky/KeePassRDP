d2d05b43e241040f3210d1a08764f95e33fd65fe
iSnackyCracky <julian@isnackycracky.de>
2020-07-29 10:45:32 +0200

    Add License (GPLv3)

--- make_release.ps1
+++ make_release.ps1
@@ -1,3 +1,20 @@
+#  Copyright (C) 2018-2020 iSnackyCracky
+#
+#  This file is part of KeePassRDP.
+#
+#  KeePassRDP is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  KeePassRDP is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 $VersionFilePath = ".\KeePassRDP.ver"
 [System.Version]$KeePassRDPver = (Get-Content "KeePassRDP\Properties\AssemblyInfo.cs" | Select-String -Pattern "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+")[0].Matches.Value
 
